5b09cf9056b68df50e2aece73e4edde471b9d90c
NTiger07 <falaleru@gmail.com>
2022-07-18 14:37:36 +0100

    app done

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,9 +1,13 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
@@ -11,9 +15,13 @@ export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]): Row[] {
     return serverResponds.map((el: any) => {
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
+        price_abc: el.price_a,
+        price_def: el.price_b,
+        ratio: el.price_abc/el.price_def,
         timestamp: el.timestamp,
+        upper_bound: 1+ 0.5,
+        lower_bound: 1+ 0.5,
+        trigger_alert: el.top_ask && el.top_ask.price || 0,
       };
     })
   }
